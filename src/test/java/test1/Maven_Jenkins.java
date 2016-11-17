6565de3087dc7b800193bc751926e6c61d4860fc
Hamid <islam.md1971@gmail.com>
2016-11-16 21:22:14 -0500

    hhh

--- src/test/java/test1/Maven_Jenkins.java
+++ src/test/java/test1/Maven_Jenkins.java
@@ -1,5 +1,19 @@
 package test1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class Maven_Jenkins {
+	WebDriver  driver =new FirefoxDriver();
+	@Test
+	
+	public void  t1(){
+		driver.get("http://www.bn.com");
+		
+		driver.close();
+		
+		
+	}
 
 }
